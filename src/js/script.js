c15a5129c85291acdba3ee94e477a57ccba4c95b
LanAd2 <tranthanhlan.ad2@gmail.com>
2022-11-19 22:59:04 +0700

    Implement the popup function.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,19 @@
 const navIcons = document.querySelector(".nav__icons");
 const nav = document.querySelector(".nav");
+const toursItemBtns = document.querySelectorAll(".tours__item-btn");
+const popup = document.querySelector(".popup");
+const popupClose = document.querySelector(".popup__close");
 
 navIcons.addEventListener("click", function () {
   nav.classList.toggle("nav--expanded");
+});
+
+toursItemBtns.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    popup.classList.add("popup--open");
+  });
+});
+
+popupClose.addEventListener("click", function () {
+  popup.classList.remove("popup--open");
 });
